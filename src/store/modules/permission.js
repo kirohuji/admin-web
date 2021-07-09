e19730f6231348bbfd75f5c1dc570dcb1b102226
ydzheng <ydzheng@hitotek.com>
2021-07-09 08:22:46 +0800

    feat: 更新

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -48,14 +48,14 @@ export function filterAsyncRoutes(routes, roles) {
 
 const state = {
   routes: [],
-  addRoutes: [],
+  addRoutes: []
 }
 
 const mutations = {
   SET_ROUTES: (state, routes) => {
     state.addRoutes = routes
     state.routes = routes
-  },
+  }
 }
 
 const actions = {
@@ -75,12 +75,12 @@ const actions = {
       commit('SET_ROUTES', accessedRoutes)
       resolve(accessedRoutes)
     })
-  },
+  }
 }
 
 export default {
   namespaced: true,
   state,
   mutations,
-  actions,
+  actions
 }
