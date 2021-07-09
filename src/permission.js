63692adf1deb6528ea2914cc92606d29eb652fd6
zhengyongda <zhengyongda@gupo.onaliyun.com>
2021-07-09 10:10:44 +0800

    feat: 更新

--- src/permission.js
+++ src/permission.js
@@ -13,7 +13,7 @@ NProgress.configure({ showSpinner: false }) // NProgress Configuration
 const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
-  // debugger
+  debugger
   // start progress bar
   NProgress.start()
 
